d21b24029853cf1b60736e97096651cef67ab85b
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-03-29 00:14:29 +0100

    Added new

--- src/main/java/com/s1935/pandahrbe/SimpleController.java
+++ src/main/java/com/s1935/pandahrbe/SimpleController.java
@@ -1,16 +1,18 @@
 package com.s1935.pandahrbe;
 
-import lombok.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SimpleController {
     @RequestMapping("/")
-    public @ResponseBody String greeting() {
+    public @ResponseBody String helloWorld() {
         return "Hello, World";
+    }
+
+    @RequestMapping("/welcome")
+    public @ResponseBody String welcome() {
+        return "Welcome";
     }
 }
