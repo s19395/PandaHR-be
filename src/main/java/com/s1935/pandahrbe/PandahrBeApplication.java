505e410ecf9d1ce0e92a336645cac4433191dbaf
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-03-28 20:47:11 +0100

    spring boot deployed

---
+++ src/main/java/com/s1935/pandahrbe/PandahrBeApplication.java
@@ -0,0 +1,13 @@
+package com.s1935.pandahrbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandahrBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandahrBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/s1935/pandahrbe/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.s1935.pandahrbe;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PandahrBeApplication.class);
+	}
+
+}
