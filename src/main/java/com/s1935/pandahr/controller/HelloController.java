3103e7ab0f625ccf107844876b4e9c7200a0ef96
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-03-31 18:30:23 +0200

    Try to fix stuff

--- src/main/java/com/s1935/pandahr/controller/HelloController.java
+++ src/main/java/com/s1935/pandahr/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.s1935.pandahr.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
     @RequestMapping("/")
-
     public String index() {
         return "Greetings from Azure Spring Apps! THIS IS THE GREEN DEPLOYMENT";
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello azure!";
+    }
 }
