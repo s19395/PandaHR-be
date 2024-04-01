39408d88769006fe66b1bf5e488fae6f339d55d5
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-04-01 22:25:51 +0200

    Testing endpoint

--- src/main/java/com/s1935/pandahr/controller/HelloController.java
+++ src/main/java/com/s1935/pandahr/controller/HelloController.java
@@ -1,10 +1,17 @@
 package com.s1935.pandahr.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 @RestController
+@CrossOrigin
 public class HelloController {
 
     @RequestMapping("/")
@@ -12,8 +19,8 @@ public class HelloController {
         return "Greetings from Azure Spring Apps! ";
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello azure!";
+    @GetMapping("/messages")
+    public ResponseEntity<List<String>> hello() {
+        return ResponseEntity.ok(Arrays.asList("Hello", "World"));
     }
 }
