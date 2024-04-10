a7161c77c1e0992c9ea5dc2971a38c0234658e03
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-04-10 18:19:02 +0200

    #3 Create RsaKey property handler and enable it in the app

--- src/main/java/com/s1935/pandahr/PandahrApplication.java
+++ src/main/java/com/s1935/pandahr/PandahrApplication.java
@@ -1,10 +1,12 @@
 package com.s1935.pandahr;
 
+import com.s1935.pandahr.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class PandahrApplication {
 
     public static void main(String[] args) {
